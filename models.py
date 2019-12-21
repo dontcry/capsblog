b292cc762b94691ee0ec2579b526e6dec945502a
李浩然 <li_haoran@hotmail.com>
2019-12-21 17:04:58 +0800

    add import

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, create_engine, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
